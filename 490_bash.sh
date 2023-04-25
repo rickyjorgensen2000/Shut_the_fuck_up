174d0388319b0165f78e80d07c7b8b55ff13fac2
Deepak Joseph <59976201+deepakj718@users.noreply.github.com>
2023-04-24 21:58:13 -0400

    Create 490_bash.sh

---
+++ 490_bash.sh
@@ -0,0 +1,38 @@
+#| Name of program : DineroIV Simulation Cache
+#| Author: Deepak Joseph    
+#| Date Created : 12/7/21
+#| Date last updated : 12/10/21
+#| Function : run four dineroIV simulations with different cache sizes
+#| Method : Using a shell script four simulations are run. Level 1 cache is now unified and level 2 same as stage 2
+
+/projects/CSE490/project2/gemTest.csh 429.mcf param1-1 1kB 1kB 1kB 1 1 1 16 > param1-1.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param1-2 2kB 1kB 1kB 1 1 1 16 > param1-2.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param1-1 4kB 1kB 1kB 1 1 1 16 > param1-3.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param1-1 8kB 1kB 1kB 1 1 1 16 > param1-4.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param1-1 16kB 1kB 1kB 1 1 1 16 > param1-5.out
+
+/projects/CSE490/project2/gemTest.csh 429.mcf param2-1 1kB 1kB 2kB 1 1 1 16 > param2-1.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param2-2 1kB 1kB 4kB 1 1 1 16 > param2-2.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param2-3 1kB 1kB 8kB 1 1 1 16 > param2-3.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param2-4 1kB 1kB 16kB 1 1 1 16 > param2-4.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param2-5 1kB 1kB 32kB 1 1 1 16 > param2-5.out
+
+/projects/CSE490/project2/gemTest.csh 429.mcf param3-1 1kB 1kB 1kB 2 1 1 16 > param3-1.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param3-2 1kB 1kB 1kB 4 1 1 16 > param3-2.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param3-3 1kB 1kB 1kB 8 1 1 16 > param3-3.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param3-4 1kB 1kB 1kB 16 1 1 16 > param3-4.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param3-5 1kB 1kB 1kB 32 1 1 16 > param3-5.out
+
+/projects/CSE490/project2/gemTest.csh 429.mcf param4-1 1kB 1kB 1kB 1 1 2 16 > param4-1.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param4-2 1kB 1kB 1kB 1 1 4 16 > param4-2.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param4-3 1kB 1kB 1kB 1 1 8 16 > param4-3.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param4-4 1kB 1kB 1kB 1 1 16 16 > param4-4.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param4-5 1kB 1kB 1kB 1 1 32 16 > param4-5.out
+
+/projects/CSE490/project2/gemTest.csh 429.mcf param5-1 1kB 1kB 1kB 1 1 1 32 > param5-1.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param5-2 1kB 1kB 1kB 1 1 1 64 > param5-2.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param5-3 1kB 1kB 1kB 1 1 1 128 > param5-3.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param5-4 1kB 1kB 1kB 1 1 1 256 > param5-4.out
+/projects/CSE490/project2/gemTest.csh 429.mcf param5-5 1kB 1kB 1kB 1 1 1 512 > param5-5.out
+
+
