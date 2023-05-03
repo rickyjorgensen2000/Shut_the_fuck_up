16d4392d29e22404c454ef90eb3d83b2397e7634
Deepak Joseph <59976201+deepakj718@users.noreply.github.com>
2023-05-03 14:40:03 -0400

    Add files via upload

--- 490_project2.sh
+++ 490_project2.sh
@@ -1,28 +1,54 @@
 #!/bin/bash
+printf "start"
 /projects/CSE490/project2/gemTest.csh 429.mcf param1-1 1kB 1kB 1kB 1 1 1 16 > param1-1.out
+printf "done1"
 /projects/CSE490/project2/gemTest.csh 429.mcf param1-2 2kB 1kB 1kB 1 1 1 16 > param1-2.out
+printf "done2"
 /projects/CSE490/project2/gemTest.csh 429.mcf param1-3 4kB 1kB 1kB 1 1 1 16 > param1-3.out
+printf "done3"
 /projects/CSE490/project2/gemTest.csh 429.mcf param1-4 8kB 1kB 1kB 1 1 1 16 > param1-4.out
+printf "done4"
 /projects/CSE490/project2/gemTest.csh 429.mcf param1-5 16kB 1kB 1kB 1 1 1 16 > param1-5.out
+printf "done5"
 /projects/CSE490/project2/gemTest.csh 429.mcf param2-1 1kB 1kB 2kB 1 1 1 16 > param2-1.out
+printf "done6"
 /projects/CSE490/project2/gemTest.csh 429.mcf param2-2 1kB 1kB 4kB 1 1 1 16 > param2-2.out
+printf "done7"
 /projects/CSE490/project2/gemTest.csh 429.mcf param2-3 1kB 1kB 8kB 1 1 1 16 > param2-3.out
+printf "done8"
 /projects/CSE490/project2/gemTest.csh 429.mcf param2-4 1kB 1kB 16kB 1 1 1 16 > param2-4.out
+printf "done9"
 /projects/CSE490/project2/gemTest.csh 429.mcf param2-5 1kB 1kB 32kB 1 1 1 16 > param2-5.out
+printf "done10"
 /projects/CSE490/project2/gemTest.csh 429.mcf param3-1 1kB 1kB 1kB 2 1 1 16 > param3-1.out
+printf "done11"
 /projects/CSE490/project2/gemTest.csh 429.mcf param3-2 1kB 1kB 1kB 4 1 1 16 > param3-2.out
+printf "done12"
 /projects/CSE490/project2/gemTest.csh 429.mcf param3-3 1kB 1kB 1kB 8 1 1 16 > param3-3.out
+printf "done13"
 /projects/CSE490/project2/gemTest.csh 429.mcf param3-4 1kB 1kB 1kB 16 1 1 16 > param3-4.out
+printf "done14"
 /projects/CSE490/project2/gemTest.csh 429.mcf param3-5 1kB 1kB 1kB 32 1 1 16 > param3-5.out
+printf "done15"
 /projects/CSE490/project2/gemTest.csh 429.mcf param4-1 1kB 1kB 1kB 1 1 2 16 > param4-1.out
+printf "done16"
 /projects/CSE490/project2/gemTest.csh 429.mcf param4-2 1kB 1kB 1kB 1 1 4 16 > param4-2.out
+printf "done17"
 /projects/CSE490/project2/gemTest.csh 429.mcf param4-3 1kB 1kB 1kB 1 1 8 16 > param4-3.out
+printf "done18"
 /projects/CSE490/project2/gemTest.csh 429.mcf param4-4 1kB 1kB 1kB 1 1 16 16 > param4-4.out
+printf "done19"
 /projects/CSE490/project2/gemTest.csh 429.mcf param4-5 1kB 1kB 1kB 1 1 32 16 > param4-5.out
+printf "done20"
 /projects/CSE490/project2/gemTest.csh 429.mcf param5-1 1kB 1kB 1kB 1 1 1 32 > param5-1.out
+printf "done21"
 /projects/CSE490/project2/gemTest.csh 429.mcf param5-2 1kB 1kB 1kB 1 1 1 64 > param5-2.out
+printf "done22"
 /projects/CSE490/project2/gemTest.csh 429.mcf param5-3 1kB 1kB 1kB 1 1 1 128 > param5-3.out
+printf "done23"
 /projects/CSE490/project2/gemTest.csh 429.mcf param5-4 1kB 1kB 1kB 1 1 1 256 > param5-4.out
+printf "done24"
 /projects/CSE490/project2/gemTest.csh 429.mcf param5-5 1kB 1kB 1kB 1 1 1 512 > param5-5.out
+printf "done25"
 
 
